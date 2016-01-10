cd0cfa099718b89e05a885036ca0a3c130e4d3fb
Rafael Possas <Rafael Possas>
2016-01-10 23:32:12 +1100

    Deleting Vendor Files

--- src/assets/js/angular-international/src/filters/template.filter.js
+++
@@ -1,6 +0,0 @@
-(function (angular) {
-    'use strict';
-    angular.module('amalieiev.international').filter('template', ['$template', function ($template) {
-        return $template;
-    }])
-}(angular));
--- src/assets/js/angular-international/src/modules/international.module.js
+++
@@ -1,4 +0,0 @@
-(function (angular) {
-    'use strict';
-    angular.module('amalieiev.international', []);
-}(angular));
